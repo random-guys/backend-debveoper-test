a0fb2b39c9234ce0e4a5a66fc0390f5ce4035afc
dchima <dcnnad@gmail.com>
2019-08-11 10:10:02 +0100

    added needed files and folders

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
+/* eslint-disable consistent-return */
 /* eslint-disable no-console */
 
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import { MongoClient as mongo } from 'mongodb';
+// routes import
+import userRouter from './routes/user';
+import teamRouter from './routes/team';
+import fixtureRouter from './routes/fixture';
 // swagger import
 
 dotenv.config();
@@ -16,10 +21,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // allow for classic url encoding
 
 // endpoint routers
-
+app.use('/api/v1/auth', userRouter);
 
 // Not Found Handler
 // app.use((req, res) => { res.status(404).send('Not Found!'); });
+
 
 // open database from mongodby
 export default new Promise(async (resolve, reject) => {
@@ -36,11 +42,6 @@ export default new Promise(async (resolve, reject) => {
       app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
     }
     return resolve(client.db());
-    /*
-        .then((client) => {
-            resolve(client.db());
-            })
-        */
   }
 });
 
