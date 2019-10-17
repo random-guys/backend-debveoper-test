bc8a7fb5611e18babd5acdad291f15ab6b841370
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-10-17 20:45:21 +0100

    add user model

---
+++ src/models/user.model.js
@@ -0,0 +1,21 @@
+const Users = [
+    {
+      user_id: 1,
+      first_name: 'Admin',
+      last_name: 'User',
+      email: 'adminuser@gmail.com',
+      password: '$2a$10$17MpQeJWeiXDlMhae/UvkO8I04nB4XOX24FnH0qN9i3VO8r9WFHni',
+      is_admin: true,
+    },
+    {
+      user_id: 2,
+      first_name: 'Regular',
+      last_name: 'User',
+      email: 'regularuser@gmail.com',
+      password: '$2a$10$17MpQeJWeiXDlMhae/UvkO8I04nB4XOX24FnH0qN9i3VO8r9WFHni',
+      is_admin: false,
+    },
+  ];
+  
+  export default Users;
+  
