d51021ad6b5559f72a825bf9dcb113d098a058c6
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-10-17 21:00:27 +0100

    add routes

--- src/index.js
+++ src/index.js
@@ -1,31 +1,53 @@
-import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import userRoute from './routes/user.route';
+import teamRoute from './routes/team.route';
+import fixtureRoute from './routes/fixture.route';
+import connectionManager from '../db.manager';
 
-// May require additional time for downloading MongoDB binaries
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
+const app = express();
+const apiVersion = '/api/v1';
 
-class TestDbHelper {
-  constructor() {
-    this.db = null;
-    this.server = new MongoMemoryServer();
-    this.connection = null;
-  }
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  }),
+);
+app.use(`${apiVersion}/users`, userRoute);
+app.use(`${apiVersion}/teams`, teamRoute);
+app.use(`${apiVersion}/fixtures`, fixtureRoute);
 
-  /**
-   * Start the server and establish a connection
-   */
-  async start() {
-    const mongoUri = await this.server.getConnectionString();
-    this.connection = await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-    });
-  }
-
-  async stop() {
-    await this.connection.disconnect();
-    return this.server.stop();
-  }
+if (process.env.NODE_ENV === 'test') {
+  connectionManager.stop();
+} else {
+  connectionManager.start();
 }
 
-export default TestDbHelper;
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: 'Welcome to the Mock Premier League',
+  });
+});
+
+app.use((err, req, res, next) => {
+  if (err instanceof URIError) {
+    return res.status(400).json({
+      status: 400,
+      error: `Failed to decode param: ${req.url}`,
+    });
+  }
+  return next();
+});
+app.use('*', (req, res) => {
+  res.status(404).json({
+    status: 404,
+    error: 'url not found',
+  });
+});
+
+export default app;
